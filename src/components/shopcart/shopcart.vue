aa8901ae77c9508477b65a57e5870cb23da36e68
Yang9322 <576421791@qq.com>
2017-06-25 21:36:44 +0800

    add bottom UI

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo">
+            <i class="icon-shopping_cart"></i>
+          </div>
+          <div class="prive"></div>
+          <div class="desc"></div>
+        </div>
+      </div>
+      <div class="content-right"></div>
+    </div>
+  </div>
+</template>
+
+
+<script type="text/ecmascript-6">
+    export default {
+        props: []
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .shopcart
+    position： fixed
+    left: 0
+    bottom: 0
+
+</style>
